277bd85ee6a3499e07d9ad3b49e17ef8f69abab1
esmurygin <smurygin_egor_ivt@mail.ru>
2022-06-12 11:51:55 +0300

    Login fields from material ui

--- components/login.js
+++ components/login.js
@@ -1,26 +1,26 @@
 import React from 'react';
 import style from '../styles/login.module.css'
-
+import TextField from '@mui/material/TextField';
 export default class login extends React.Component {
-    constructor(props){
+    constructor(props) {
         super(props);
         this.loginChange = this.loginChange.bind(this)
         this.passwordChange = this.passwordChange.bind(this)
         this.handleSubmit = this.handleSubmit.bind(this)
-        this.state={ login:"", password:""}
+        this.state = { login: "", password: "" }
     }
 
     loginChange(event) {
-        this.setState({login : event.target.value})
-        
+        this.setState({ login: event.target.value })
+
     }
 
-    passwordChange(event){
-        this.setState({password : event.target.value})
+    passwordChange(event) {
+        this.setState({ password: event.target.value })
     }
 
-    
-    handleSubmit(event){
+
+    handleSubmit(event) {
         event.preventDefault();
         fetch('https://i-pro-backend.herokuapp.com/login', {
             method: 'POST',
@@ -28,35 +28,33 @@ export default class login extends React.Component {
                 'Content-Type': 'application/x-www-form-urlencoded'
             },
             body: `grant_type=&username=${this.state.login}&password=${this.state.password}&scope=&client_id=&client_secret=`
-        }).then((res)=>{
-            if(res.status == 200){
-                res.json().then((res)=>{
+        }).then((res) => {
+            if (res.status == 200) {
+                res.json().then((res) => {
                     alert(res['access_token'])
                 })
-            }else{
-                res.json().then((res)=>{
+            } else {
+                res.json().then((res) => {
                     alert(res['detatils']);
                 })
             }
         })
     }
-
-
-    render(){
-        return(
+    render() {
+        return (
             <form onSubmit={this.handleSubmit} className={style.login}>
                 <h1 className={style.hed1special}>Nashe Delo</h1>
                 <p className={style.par}>Ещё нет аккаунта? <a className={style.link}>Зарегестрироваться</a></p>
                 <h1 className={style.hed1}>Вход</h1>
-                <input className={style.inpLogin} value={this.state.login} placeholder="Электропочта*" onChange={this.loginChange}/>
-                <input className={style.inpPassword} value={this.state.password} placeholder="Пароль*" onChange={this.passwordChange}/>
+                <TextField className={style.inpLogin} id="outlined-basic" label="Электропочта*" variant="outlined" value={this.state.login} onChange={this.loginChange} />
+                <TextField className={style.inpPassword} id="outlined-basic" label="Пароль*" variant="outlined" value={this.state.password} onChange={this.passwordChange} />
                 <div className={style.rowSpecial}>
                     <input className={style.check} type="checkbox" id="memory"/><label htmlFor="memory" className={[style.lbl, style.extended].join(' ')}>Запомнить меня</label>
                     <a className={[style.link2, style.fontRegular].join(' ')}>Забыли пароль?</a>
                 </div>
-                <input type='submit' value='Войти' className={style.button}/>
-                <input type='submit' value='Регистрация организатора' className={style.button2}/>
+                <input type='submit' value='Войти' className={style.button} />
+                <input type='submit' value='Регистрация организатора' className={style.button2} />
             </form>
-        )      
+        )
     }
 }
