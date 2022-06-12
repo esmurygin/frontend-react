686cf2a152219f7ecde2a144a6cad441ebef714d
Tatyana <tanuceerveevna@gmail.com>
2022-06-13 00:13:47 +0300

    added navbar component

---
+++ components/navbar.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import style from '../styles/navbar.module.css'
+
+export default class Navbar extends React.Component {
+    constructor(props) {
+        super(props);
+       
+    }
+
+    render() {
+        return (
+            <div>
+                <div className={style.navigationPanelWrapper}>
+                    <nav className={style.navigationPanel}>
+                        <div>
+                            <a href="#" className={[style.navigationPanelItem, style.navigationPanelItemLogo].join(' ')}>
+                                Nashe Delo
+                            </a>
+                        </div>
+                        <div className={style.navigationPanelSpacer}></div>
+                        <div>
+                            <a href="#" className={[style.navigationPanelItem, style.navigationPanelItemSelected].join(' ')}>
+                                Главная
+                            </a>
+                            <a href="" className={style.navigationPanelItem}>
+                                Наша карта
+                            </a>
+                            <a href=""  className={style.navigationPanelItem}>
+                                FAQ
+                            </a>
+                            <form className={[style.navigationPanelItem, style.search].join(' ')} action="">
+                                <input type="search" placeholder="Найдите доброе дело" required />
+                                <a href="">
+                                    <img src={"../styles/svg/mainPage_searchIcon.svg"} alt="" />
+                                </a>
+                            </form>
+                            <a href="#">
+                                <button className={[style.navigationPanelItem, style.navigationPanelLogin].join(' ')}>Войти</button>
+                            </a>
+                            <a href="#">
+                                <button className={[style.navigationPanelItem, style.navigationPanelRegister].join(' ')}>Зарегистрироваться</button>
+                            </a>
+                        </div>
+                    </nav>
+                </div>
+            </div>
+        )
+    }
+}
---
+++ pages/navbar.js
@@ -0,0 +1,9 @@
+import Navbar from '../components/navbar'
+
+export default function logimnPage() {
+  return (
+        <div style={{width: '100%', height: '100%'}}>
+             <Navbar></Navbar>
+        </div>
+  )
+}
