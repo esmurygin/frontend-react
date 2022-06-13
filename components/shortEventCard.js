d1a2606d1d6b20b692f16d4e8da643e23c13569a
esmurygin <smurygin_egor_ivt@mail.ru>
2022-06-13 07:04:26 +0300

    Compose search page

--- components/shortEventCard.js
+++ components/shortEventCard.js
@@ -11,22 +11,24 @@ export default class ShortEventCard extends React.Component {
     render() {
         return (
             <div className={style.volunteerCard}>
-                <h4 className={style.cardSubtitle}>Отбор волонтёров на экологическую смену на базе Национального парка</h4>
-                <div className={style.volunteerVolunteer}>Россия, г. Москва</div>
+
+                <div className={style.flexRow}>
+                <img src={'/static/greyLike.png'} style={{position:'absolute', right:"1rem", top:"1rem" }}></img>
+
+                    <h4 className={style.cardSubtitle}>Отбор волонтёров на экологическую смену на базе Национального парка</h4>
+                </div>
+                <div className={style.volunteerVolunteer}><img src={"/static/calendarEventPage.svg"} style={{ width: '12px', marginRight: '12px' }}></img> 17 Мая - 4 Июля, 16:00</div>
+                <div className={style.volunteerVolunteer}><img src={"/static/mapEventPage.svg"} style={{ width: '12px', marginRight: '12px' }}></img> Россия, г. Москва</div>
                 <div className={style.cardContent}>
-                <img src={"/static/heart.svg"} className={style.bookMark}></img>
                     <div className={style.upper}>
-                        <img src={this.props.image} className={style.volunteerCardImage} />
-                        
+                        <img src={this.props.image} className={style.eventCardImage} />
                     </div>
-                    <h4 className={style.titleCard}>Направления:</h4>
+                    <div style={{ height: '2rem' }}></div>
                     <div className={style.containerFlexRow}>
-                        <div className={[style.directionGeneral, style.directionOrange].join(' ')}>Здравоохранение</div>
-                        <div className={[style.directionGeneral, style.directionPink].join(' ')}>Социальное</div>
-                        <div className={[style.directionGeneral, style.directionBlue].join(' ')}>Событийное</div>
+                        <div className={[style.directionGeneral, style.directionOrange].join(' ')} style={{ fontSize: '11px' }}>Здравоохранение</div>
+                        <div className={[style.directionGeneral, style.directionPink].join(' ')} style={{ fontSize: '11px' }}>Социальное</div>
+                        <div className={[style.directionGeneral, style.directionBlue].join(' ')} style={{ fontSize: '11px' }}>Событийное</div>
                     </div>
-                    
-                    
                 </div>
             </div>
         )
---
+++ pages/legacy/cards_test_2.js
@@ -0,0 +1,30 @@
+import VolunteerCard from '../components/volunteerCard'
+import Footer from '../components/footer'
+import style from '../styles/volunteerCard.module.css'
+import SearchFilters from '../components/searchFilters';
+
+export default function testCards() {
+   return (
+      <div style={{backgroundColor: '#EFF1FB'}}>
+         <div style={{ width: '100%', height: '100%', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+            <SearchFilters></SearchFilters>
+            {/* { <VolunteerCard image={"/static/denis.png"}></VolunteerCard> } */}
+            {/* <div style={{
+                position: 'fixed',
+                bottom: '0.25em',
+                width: '100%',
+                textAlign: 'center',
+                fontFamily: 'regular'
+             }}>© I PRO. MoscowCityHack 2022</div>
+             <div style={{
+                position: 'fixed',
+                bottom: '0.25em',
+                right: '7em',
+                textAlign: 'center'
+             }}></div> */}
+         </div>
+         <Footer></Footer>
+      </div>
+
+   )
+}
